340e465ed4ea37125d58b6234c6e22a977246982
Donko Dimov <donko@abv.bg>
2020-06-04 13:21:01 +0200

    Create setup.sh

---
+++ scripts/install/setup.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+dotfilesDir=$(pwd)
+
+function linkDotfile {
+  dest="${HOME}/${1}"
+  dateStr=$(date +%Y-%m-%d-%H%M)
+
+  if [ -h ~/${1} ]; then
+    # Existing symlink 
+    echo "Removing existing symlink: ${dest}"
+    rm ${dest} 
+
+  elif [ -f "${dest}" ]; then
+    # Existing file
+    echo "Backing up existing file: ${dest}"
+    mv ${dest}{,.${dateStr}}
+
+  elif [ -d "${dest}" ]; then
+    # Existing dir
+    echo "Backing up existing dir: ${dest}"
+    mv ${dest}{,.${dateStr}}
+  fi
+
+  echo "Creating new symlink: ${dest}"
+  ln -s ${dotfilesDir}/${1} ${dest}
+}
+
+linkDotfile .vim
+linkDotfile .vimrc
+linkDotfile .bashrc
+
+
+
+# Download Vim Plug:
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
